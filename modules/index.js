ceed33832f66be55fb95b46f459234d6a0bb4c44
Steve Ward <stevewardiress@users.noreply.github.com>
2015-03-17 01:04:50 +0000

    Fixed the authenticate user method

--- modules/index.js
+++ modules/index.js
@@ -1,9 +1,11 @@
 module.exports = {
     authenticateUser: function (req, res, next) {
+        res.locals.isAuthenticated = false;
         if (req.query.token) {
             //  validate the token here :-)
             res.locals.isAuthenticated = true;
         }
+        console.log("isAuthenticated = %s", res.locals.isAuthenticated)
         next();
     },
 

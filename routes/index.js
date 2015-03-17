dd7990c82c5ea8e4d6a634bf50d4e1baef451525
Steve Ward <stevewardiress@users.noreply.github.com>
2015-03-17 00:49:13 +0000

    Fix bug with the match all route for the middleware

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 module.exports = function(app) {
 
+    //  this function runs for any request on any url
+    app.all("*", function (req, res, next) {
+        console.log("middleware running for url %s", req.url);
+        next();
+    });
+
     //  load all of our routers and add them to the application
     app.use("/api", require("./api"));
     app.use("/", require("./www"));
-
-    //  this function runs for any request on any url
-    app.all("/", function (req, res, next) {
-        res.write("middleware woz ere<br>");
-        next();
-    });
 
     //  handle any other requests
     app.use(function (req, res) {

386761918c79a7fc35fd2d2714d06c130535d077
Steve Ward <stevewardiress@users.noreply.github.com>
2015-03-17 01:00:04 +0000

    Fixed the authenticate user method

--- routes/www/index.js
+++ routes/www/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var middleWare = require("../../modules");
 
 router.use(express.static('public'));
+
+// authenticate the user if there's a token, but don't force them to login
+router.use(middleWare.authenticateUser);
+
 module.exports = router;

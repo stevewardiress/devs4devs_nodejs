6cd9c7fca43c7c0c5a3cea69abf76da55991f447
Steve Ward <stevewardiress@users.noreply.github.com>
2015-03-17 00:55:13 +0000

    Fix bug with the match all route for the middleware

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 module.exports = function(app) {
 
-//  this function runs for any request on any url
-    app.all("/", function (req, res, next) {
-        res.write("middleware woz ere<br>");
+    //  this function runs for any request on any url
+    app.all("*", function (req, res, next) {
+        console.log("middleware running for url %s", req.url);
         next();
     });
 
-//  handle any other requests
+    //  handle any other requests
     app.use(function (req, res) {
         res.status(404).write('Sorry cant find that!');
         res.end();
